ea9546c5aa27577e87f2fa463f82ce47242619ab
Boburjon04 <63533094+Boburjon04@users.noreply.github.com>
2021-05-14 15:24:43 +0500

    Add files via upload

---
+++ config.py
@@ -0,0 +1,10 @@
+import  os
+
+BOT_TOKEN = os.environ.get("1846804384:AAGFdjo4olUzuvOc1WAJyMw3AXf1aPaSkxY")
+APP_ID = int(os.environ.get("715501"))
+API_HASH = os.environ.get("fea66b2fb63c10305df5eb52bbf6b737")
+
+youtube_next_fetch = 0  # time in minute
+
+
+EDIT_TIME = 5

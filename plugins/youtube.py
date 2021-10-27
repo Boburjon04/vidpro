924901b7dbd55c9190555a0482c05492078a6cac
Boburjon Ravzatov <63533094+Boburjon04@users.noreply.github.com>
2021-10-27 17:52:42 +0500

    Add files via upload

--- plugins/youtube.py
+++ plugins/youtube.py
@@ -5,7 +5,10 @@ from config import youtube_next_fetch
 from helper.ytdlfunc import extractYt, create_buttons
 import wget
 import os
+import time
 from PIL import Image
+from plugins.display_progress import progress_for_pyrogram, humanbytes, TimeFormatter
+from config import Config
 
 ytregex = r"^((?:https?:)?\/\/)?((?:www|m)\.)?((?:youtube\.com|youtu.be))(\/(?:[\w\-]+\?v=|embed\/|v\/)?)([\w\-]+)(\S+)?$"
 
@@ -45,7 +48,34 @@ async def ytdl(_, message):
             os.makedirs(output_directory)
         thumb_image_path = f"{output_directory}.jpg"
         im.save(thumb_image_path,"jpeg")
-        await message.reply_photo(thumb_image_path, caption=title, reply_markup=buttons)
+                     if round(diff % 5.00) == 0 or downloaded == total_length:
+                    percentage = downloaded * 100 / total_length
+                    speed = downloaded / diff
+                    elapsed_time = round(diff) * 1000
+                    time_to_completion = round(
+                        (total_length - downloaded) / speed) * 1000
+                    estimated_total_time = elapsed_time + time_to_completion
+                    try:
+                        current_message = """**Download Status**
+File Size: {}
+Downloading: {}
+ETA: {}""".format(
+    url,
+    humanbytes(total_length),
+    humanbytes(downloaded),
+    TimeFormatter(estimated_total_time)
+)
+                        if current_message != display_message:
+                            await bot.edit_message_text(
+                                chat_id,
+                                message_id,
+                                text=current_message
+                            )
+                            display_message = current_message
+                    except Exception as e:
+                        logger.info(str(e))
+                        pass
+        return await response.release()
         await sentm.delete()
     except Exception as e:
         print(e)
